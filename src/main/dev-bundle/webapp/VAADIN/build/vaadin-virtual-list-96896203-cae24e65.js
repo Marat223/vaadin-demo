fa1ed3f0b6dbc628b5daa2b6469dbeda6c7a67dd
Mustafin.M <Mustafin.M@mintrud.by>
2023-10-25 20:04:37 +0300

    Первый коммит

---
+++ src/main/dev-bundle/webapp/VAADIN/build/vaadin-virtual-list-96896203-cae24e65.js
@@ -0,0 +1,1 @@
+import{R as r}from"./indexhtml-8f52a0d8.js";const t={tagName:"vaadin-virtual-list",displayName:"Virtual List",elements:[{selector:"vaadin-virtual-list",displayName:"List",properties:[r.backgroundColor,r.borderColor,r.borderWidth,r.borderRadius]}]};export{t as default};
---
+++ src/main/java/com/example/application/views/test/TestView.java
@@ -0,0 +1,30 @@
+package com.example.application.views.test;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("Тестовое представление")
+@Route(value = "test", layout = MainLayout.class)
+public class TestView extends HorizontalLayout {
+
+    public TestView() {
+
+        Button button = new Button("Нажми");
+        DatePicker datePicker = new DatePicker("Укажите дату");
+
+        button.addClickListener(event -> {
+            add(new Text("Кнопка нажата"));
+            Notification.show("Нажато!");
+        });
+
+        setVerticalComponentAlignment(Alignment.END, button, datePicker);
+
+        add(button, datePicker);
+    }
+}
